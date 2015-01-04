7cec40ea1589360f26bc0c16f4e055c2b45562ff
geetanjaligg <anjaligg@gmail.com>
2015-01-04 18:32:05 +0000

    flatten json before indexing. Mapping error

--- parse_xml.py
+++ parse_xml.py
@@ -12,8 +12,19 @@ import cybox.bindings.cybox_core as cybox_core_binding
 from  cybox.core import Observables
 import json
 import rawes
+import collections
 
 es = rawes.Elastic('http://localhost:9200')
+
+def flatten(d, parent_key='', sep='_'):
+    items = []
+    for k, v in d.items():
+        new_key = parent_key + sep + k if parent_key else k
+        if isinstance(v, collections.MutableMapping):
+            items.extend(flatten(v, new_key, sep=sep).items())
+        else:
+            items.append((new_key, v))
+    return dict(items)
 
 def parse(xml_file):
     observables_obj = cybox_core_binding.parse(xml_file) # create binding object from xml file
@@ -30,19 +41,20 @@ def main():
         print "[!] Please provide an xml file" 
         exit(1)
     
-    print 'HELLLLLLLLLLLOOOOOOOOOOO'
-    #xml_file = sys.argv[-1]
     xml = sys.argv[-1]
-    #observables = parse(xml_file) 
     observables = parseString(xml)
-    #print observables.to_dict() # example to_dict() call on returned object
     ob_dict = observables.to_dict()
-    #print json.dumps(ob_dict) #print json
-    properties = ob_dict['observables'][0]['object']['properties']
-    del ob_dict['observables'][0]['object']['properties']
-    ob_dict['observables'][0]['object']['properties'] = {}
-    ob_dict['observables'][0]['object']['properties']['properties'] = properties
-    ob_dict['observables'][0]['object']['properties']['type'] = 'nested'
+    #print "length of observables   " + str(len(ob_dict['observables']))
+    if(len(ob_dict['observables'])>1):
+        for i,val in enumerate(ob_dict['observables']):
+            if('object' in ob_dict['observables'][i]):
+                flat_ob_dict = flatten(ob_dict['observables'][i]['object']['properties'])    
+                ob_dict['observables'][i]['object']['properties'] = flat_ob_dict
+
+    elif('object' in ob_dict['observables'][0]):
+        flat_ob_dict = flatten(ob_dict['observables'][0]['object']['properties'])    
+        ob_dict['observables'][0]['object']['properties'] = flat_ob_dict
+    
     ob_json = json.dumps(ob_dict)
     #print ob_json
     try:
